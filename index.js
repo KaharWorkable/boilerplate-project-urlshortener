92ffe1a29fa6c10533a5e3367ba1f6698819d36b
Kahar <kaharmuslim12@gmail.com>
2023-03-12 23:50:14 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const app = express();
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
-//url shorterner
-
 app.use(cors());
+
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -16,11 +16,36 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+const originalurl = [];
+const shorturl = [];
+
 // Your first API endpoint
-app.get('/api/shorturl/:short', function(req, res) {
-  var short = req.params.short;
-  res.json({ original_url: 'https://www.google.com', short_url: short });
-  
+app.post('/api/shorturl/', (req, res) => {
+  const url = req.body.url;
+  const foundIndex = originalurl.indexOf(url);
+
+  if (url.includes('http://') || url.includes('https://')) {
+    if (foundIndex < 0) {
+      originalurl.push(url);
+      shorturl.push(originalurl.length - 1);
+      res.json({ original_url: url, short_url: originalurl.length - 1 });
+    } else {
+      res.json({ original_url: url, short_url: foundIndex });
+    }
+  } else {
+    res.json({ error: 'invalid url' });
+  }
+})
+
+app.get('/api/shorturl/:short', (req, res) => {
+  const short = parseInt(req.params.short);
+  const foundIndex = shorturl.indexOf(short);
+
+  if (foundIndex < 0) {
+    return res.json({ error: 'invalid url' });
+  } else {
+    return res.redirect(originalurl[foundIndex]);
+  }
 });
 
 app.listen(port, function() {
